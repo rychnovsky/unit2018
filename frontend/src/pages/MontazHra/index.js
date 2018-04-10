5def20276d120898d168f7f7293d3c1f585449be
Filip Štěrba <sterbfil@fel.cvut.cz>
2018-04-10 16:11:02 +0200

    + montaz

--- frontend/src/pages/MontazHra/index.js
+++ frontend/src/pages/MontazHra/index.js
@@ -2,19 +2,50 @@ import React, { Component } from 'react'
 import GlobalWrap from '../../components/GlobalWrap'
 import Logo from '../../components/Logo'
 import { H1 } from '../../common/typography'
-import { ImageWrap } from './styled'
+import { Img, Button, BtnWrap, TrioWrap } from './styled'
+import Img1 from '../../assets/img/komplet/parts_back_lights.svg'
+import Img2 from '../../assets/img/komplet/parts_bear.svg'
+import Img3 from '../../assets/img/komplet/parts_cake.svg'
+import Img4 from '../../assets/img/komplet/parts_front_lights.svg'
+import Img5 from '../../assets/img/komplet/parts_shit.svg'
+import Img6 from '../../assets/img/komplet/parts_wheels.svg'
+
+import imgmain from '../../assets/img/komplet/01_main_picture.svg'
+
+import SkodikHelp from '../../components/SkodikHelp'
 
 class MontazHra extends Component {
-
-
-
   render() {
     return (
       <GlobalWrap>
         <Logo />
         <H1>Pomoz nám namontovat díly</H1>
-        <ImageWrap />
-
+        <Img src={imgmain} />
+        <BtnWrap>
+          <TrioWrap>
+            <Button>
+              <img src={Img1} />
+            </Button>
+            <Button>
+              <img src={Img2} />
+            </Button>
+            <Button>
+              <img src={Img3} />
+            </Button>
+          </TrioWrap>
+          <TrioWrap>
+            <Button>
+              <img src={Img4} />
+            </Button>
+            <Button>
+              <img src={Img5} />
+            </Button>
+            <Button>
+              <img src={Img6} />
+            </Button>
+          </TrioWrap>
+        </BtnWrap>
+        <SkodikHelp />
       </GlobalWrap>
     )
   }
--- frontend/src/pages/MontazHra/styled.js
+++ frontend/src/pages/MontazHra/styled.js
@@ -1,7 +1,22 @@
 import styled from 'styled-components'
 
-
-export const ImageWrap = styled.div `
+export const Img = styled.img `
   width: 100%;
   height: 300px;
+`
+export const Button = styled.button `
+  width: 90px;
+  height: 90px;
+  background: #ffffff;
+  border: 1px solid #626262;
+  box-sizing: border-box;
+  border-radius: 2px;
+  margin: 5px;
+`
+export const TrioWrap = styled.div `
+  display: flex;
+`
+export const BtnWrap = styled.div `
+  display: flex;
+  flex-direction: column;
 `
