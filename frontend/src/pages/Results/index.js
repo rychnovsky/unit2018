b7c3c27b4247127499242815e0ecf36c75c7e85e
Filip Štěrba <sterbfil@fel.cvut.cz>
2018-04-10 15:52:33 +0200

    svarovna gif

--- frontend/src/pages/Results/index.js
+++ frontend/src/pages/Results/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Wrap } from './styled'
 import CTAlink from '../../components/CTAlink'
 import { H1, H2, Text } from '../../common/typography'
 import Logo from '../../components/Logo'
@@ -7,14 +6,20 @@ import GlobalWrap from '../../components/GlobalWrap'
 
 import Badge from '../../assets/img/badge.svg'
 
+import ImageSrc from '../../assets/img/kontrolaHra/05_quiz_Choice4_RIGHT.svg'
+
 class Homepage extends Component {
   render() {
     return (
       <GlobalWrap>
         <Logo />
-        <H1>Skvělá práce, jsi šikula! Děkuji ti!</H1>
+        <H1 center>Skvělá práce, jsi šikula! Děkuji&nbsp;ti!</H1>
         <img src={Badge} />
-        <Text center>Tohle je naše nové auto, pěkné, že? Chceš se podívat, jak to v takové velké továrně vypadá ve skutečnosti? Pojď si prohlédnout galerii!</Text>
+        <img src={ImageSrc} />
+        <Text center>
+          Tohle je naše nové auto, pěkné, že? Chceš se podívat, jak to v takové velké továrně vypadá
+          ve skutečnosti? Pojď si prohlédnout galerii!
+        </Text>
         <CTAlink to="https://google.com" restricted>
           Prohlédnout galerii
         </CTAlink>
