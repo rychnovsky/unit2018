0d068e5875b9a2aab6c0978a59ef9b0e1f03a259
Filip Štěrba <sterbfil@fel.cvut.cz>
2018-04-10 10:09:30 +0200

    second page setup

--- frontend/src/pages/Homepage/index.js
+++ frontend/src/pages/Homepage/index.js
@@ -4,11 +4,13 @@ import { Wrap } from './styled'
 import CTAlink from '../../components/CTAlink'
 import { H1, H2 } from '../../common/typography'
 import Skodik from '../../components/Skodik'
+import Logo from '../../components/Logo'
 
 class Homepage extends Component {
   render() {
     return (
       <Wrap>
+        <Logo />
         <H1>Výroba auta se škodíkem</H1>
         <Skodik />
         <H2>Ahoj, já jsem Škodík!</H2>
