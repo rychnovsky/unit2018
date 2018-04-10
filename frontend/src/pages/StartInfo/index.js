4a4e8a95e10c1988ed7193d04dd2b5d27547624a
Vojtěch Rychnovský <rychnvoj@fel.cvut.cz>
2018-04-10 13:50:22 +0200

    First two slides

--- frontend/src/pages/StartInfo/index.js
+++ frontend/src/pages/StartInfo/index.js
@@ -4,21 +4,22 @@ import Logo from '../../components/Logo'
 import { H1, Text } from '../../common/typography'
 import Skodik from '../../components/Skodik'
 import SkodikBtn from '../../components/SkodikBtn'
+import GlobalWrap from '../../components/GlobalWrap'
 
 class StartInfo extends Component {
   render() {
     return (
-      <Wrap>
+      <GlobalWrap>
         <Logo />
         <H1>Jsem tu pořád pro tebe!</H1>
         <Skodik />
-        <Text>
+        <Text center>
           Kdykoliv si nebudeš vědět s něčím rady, budu tady pro tebe, stačí na mě kliknout v pravém
           dolním rohu.
         </Text>
-        <Text>Pojďme to zkusit, klikni na mě!</Text>
-        <SkodikBtn to="lisovna">Klikni na me</SkodikBtn>
-      </Wrap>
+        <Text center>Pojďme to zkusit, klikni na mě!</Text>
+        <SkodikBtn to="lisovna">Klikni na mě</SkodikBtn>
+      </GlobalWrap>
     )
   }
 }
