a25b16bd579bb17c47d9a689f6a8f0e175149270
Alexander Mensák <mensak@easycoreapps.com>
2018-04-10 14:21:58 +0200

    skodik help

--- frontend/src/pages/LisovnaHra/index.js
+++ frontend/src/pages/LisovnaHra/index.js
@@ -9,7 +9,13 @@ class LisovnaHra extends Component {
 
   answer = 0;
 
+  constructor(props) {
+    super(props);
+    this.state = {success: null};
+  }
+
   onSelect = (position) => {
+    console.log("selected");
     if (position == this.answer){
       this.onSuccess();
     } else {
@@ -18,12 +24,13 @@ class LisovnaHra extends Component {
   };
 
   onSuccess = () => {
+    this.setState({success: true});
     //TODO make answer box green
     //TODO show popup and redirect - onNext();
   };
 
   onFail = (position) => {
-
+    this.setState({success: false});
   };
 
   onNext = () => {
@@ -31,6 +38,32 @@ class LisovnaHra extends Component {
   };
 
   render() {
+
+    console.log("rendeer " + this.state.success);
+    let skodik;
+    if (this.state.success === true){
+      skodik = <SkodikHelp
+          visible={true}
+          linkText="Pokračovat"
+          title1="Správně, skvělá práce!"
+          text1="Chceš pokračovat dále?"/>
+
+    } else if (this.state.success === false){
+      skodik = <SkodikHelp
+        visible={true}
+        linkText="Zkusit znovu"
+        title1="Kousek vedle, zkus to znovu"/>
+
+    } else {
+      skodik = <SkodikHelp
+          visible={false}
+          linkText="Už vím!"
+          title1="Ovládání hry"
+          text1="Jen zmáčkni vybranou karoserii a škodík ti řekne, jestli je správná"
+          title2="Jak vypadá karoserie?"
+          text2="Copak jsi nikdy neviděl auto?"/>;
+    }
+
     return (
       <GlobalWrap>
         <Logo />
@@ -38,17 +71,17 @@ class LisovnaHra extends Component {
         <ImageWrap />
         <BtnWrap>
           <TrioWrap>
-            <Button onClick={this.onSelect(0)} />
-            <Button onClick={this.onSelect(1)} />
-            <Button onClick={this.onSelect(2)} />
+            <Button onClick={() => this.onSelect(0)} />
+            <Button onClick={() => this.onSelect(1)} />
+            <Button onClick={() => this.onSelect(2)} />
           </TrioWrap>
           <TrioWrap>
-            <Button onClick={this.onSelect(3)} />
-            <Button onClick={this.onSelect(4)} />
-            <Button onClick={this.onSelect(5)} />
+            <Button onClick={() => this.onSelect(3)} />
+            <Button onClick={() => this.onSelect(4)} />
+            <Button onClick={() => this.onSelect(5)} />
           </TrioWrap>
+          {skodik}
         </BtnWrap>
-        <SkodikHelp/>
       </GlobalWrap>
     )
   }
